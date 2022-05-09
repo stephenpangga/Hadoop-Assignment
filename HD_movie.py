93af35b37e75c1e1ff53d173749512bbfab18418
Stephen Pangga <42969061+stephenpangga@users.noreply.github.com>
2022-05-09 22:40:28 +0200

    adding comments and link

--- HD_movie.py
+++ HD_movie.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
@@ -5,17 +6,44 @@ class MoviesBreakdown(MRJob):
     def steps(self):
         return[
             MRStep(
-                mapper = self.mapper_get_movies,
-                reducer = self.reducer_count_movies
+                mapper = self.mapper_get_ratings,
+                combiner = self.combine_movie_rating_and_add,
+                reducer = self.reducer_count_ratings
+            ),
+            MRStep(
+                reducer = self.reducer_sort_counts
             )
         ]
 
-    def mapper_get_movies(self,_,line):
-        (movieID, movieName, movieData, link)=line.split(',')
-        yield movieID, 4
+    # The mapper() method takes a key and a value as args
+    # in this case, the key is ignored and a single line of text input is the value) 
+    # and yields as many key-value pairs as it likes.
+    def mapper_get_ratings(self, _, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield movieID, 1
 
-    def reducer_count_movies(self, key, values):
+    # The Combiner
+    # takes the a key, and subset the values for that key and return a key-value pairs
+    # this will optimize the run after running the mapper method
+    # can be used to decrease the total data transfer.
+    def combine_movie_rating_and_add(self, key,values):
         yield key, sum(values)
+
+
+    # The reducer method takes all the key-values and counts all the values 
+    # and add them together toi the same key which then show the total
+    # value for each rating key
+    # The sum will add up and give total 
+    def reducer_count_ratings (self, key, values):
+        yield None, (sum(values), key)
+
+    #sort
+    def reducer_sort_counts(self, _, values):
+        for count, key in sorted(values, reverse=True):
+            yield int(key), count
+
 
 if __name__ == '__main__':
     MoviesBreakdown.run()
+
+    ##example code: https://stackoverflow.com/questions/53707962/mrjob-sort-reducer-output
